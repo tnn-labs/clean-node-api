9d1adcfc560cc9a77d2ff35caece139385ae5f40
theandersonn <theandersonn@gmail.com>
2022-02-24 17:56:56 -0300

    feat: ensure requests parse body as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-import express from 'express';
+import app from './config/app';
 
-const app = express();
 app.listen(5050, () => console.log('Server running at http://localhost:5050'));
