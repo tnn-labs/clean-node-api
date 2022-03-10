c651c5230f2832904dd887572986e75e234b98fe
theandersonn <theandersonn@gmail.com>
2022-03-10 14:01:54 -0300

    refactor: make server error return the error stack

--- src/presentation/errors/server-error.ts
+++ src/presentation/errors/server-error.ts
@@ -1,8 +1,9 @@
 export class ServerError extends Error {
-  constructor() {
+  constructor(stack?: string) {
     // as classes que extendem de Error precisam invocar o método super
     super('Internal server error');
     // é uma boa pratica setar o this.name com o nome da classe
-    this.name = 'MissingParamError';
+    this.name = 'ServerError';
+    this.stack = stack;
   }
 }
