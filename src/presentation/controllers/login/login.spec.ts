ac3035506f9eeb5e1c0d1de4786cedf712088953
theandersonn <theandersonn@gmail.com>
2022-03-13 15:30:04 -0300

    test: ensure loginController calls EmailValidator correct email

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,15 +1,28 @@
 import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
+import { EmailValidator } from '../signup/signup-protocols';
 import { LoginController } from './login';
 
 interface SutTypes {
   sut: LoginController;
+  emailValidatorSub: EmailValidator;
 }
 
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
+
 const makeSut = (): SutTypes => {
-  const sut = new LoginController();
+  const emailValidatorSub = makeEmailValidator();
+  const sut = new LoginController(emailValidatorSub);
   return {
     sut,
+    emailValidatorSub,
   };
 };
 
@@ -18,7 +31,7 @@ describe('Login Controller', () => {
     const { sut } = makeSut();
     const httpRequest = {
       body: {
-        // email: 'any_email',
+        // email: 'any_email@email.com',
         password: 'any_password',
       },
     };
@@ -31,11 +44,26 @@ describe('Login Controller', () => {
     const { sut } = makeSut();
     const httpRequest = {
       body: {
-        email: 'any_email',
+        email: 'any_email@email.com',
         // password: 'any_password',
       },
     };
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')));
+  });
+
+  // esse teste garante a integração correta entre os componentes `LoginController` e `EmailValidator`
+  // quando o handle do `sut` for chamado, internamente ele chamará o `isValid`
+  test('Should call EmailValidator with correct email', async () => {
+    const { sut, emailValidatorSub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorSub, 'isValid');
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password',
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@email.com');
   });
 });
