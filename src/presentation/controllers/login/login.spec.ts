b097ce6d9d6d122b997a0c7bb5374e5a3f957e98
theandersonn <theandersonn@gmail.com>
2022-03-15 11:13:45 -0300

    refactor: group LoginController dependencies in a single file

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,13 +1,11 @@
-import { Authentication } from '../../../domain/usecases/authentication';
 import { InvalidParamError, MissingParamError } from '../../errors';
 import {
   badRequest,
   serverError,
   unauthorized,
 } from '../../helpers/http-helper';
-import { HttpRequest } from '../../protocols';
-import { EmailValidator } from '../signup/signup-protocols';
 import { LoginController } from './login';
+import { EmailValidator, Authentication, HttpRequest } from './login-protocols';
 
 interface SutTypes {
   sut: LoginController;
