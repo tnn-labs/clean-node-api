fb673baf4dc0d28cc797f271b75930f0641872c4
theandersonn <theandersonn@gmail.com>
2022-03-19 10:50:27 -0300

    chore: config rule eslint no-restricted-syntax

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -26,7 +26,6 @@ export class LoginController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFieds = ['email', 'password'];
-      // eslint-disable-next-line no-restricted-syntax
       for (const field of requiredFieds) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field));
