62034b54eeeb6c54d4e3dac94429bcfe8fee4a42
theandersonn <theandersonn@gmail.com>
2022-03-29 15:05:11 -0300

    chore: add env domain

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,4 +2,5 @@ export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H',
+  domain: process.env.DOMAIN || 'http://localhost',
 };
--- src/main/server.ts
+++ src/main/server.ts
@@ -6,7 +6,7 @@ MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default;
     app.listen(5050, () =>
-      console.log(`Server running at http://localhost:${env.port}`),
+      console.log(`Server running at ${env.domain}:${env.port}`),
     );
   })
   .catch(console.error);
