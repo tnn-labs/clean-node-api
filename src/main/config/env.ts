f791ac6c1d7bc5c0f850620e6b7d95bc21c216a3
theandersonn <theandersonn@gmail.com>
2022-03-30 10:46:43 -0300

    chore: fix port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 8080,
+  port: process.env.PORT || 3333,
   jwtSecret: process.env.JWT_SECRET || 'tj670==5H',
   domain: process.env.DOMAIN || 'http://localhost',
 };
